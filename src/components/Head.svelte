150ce72e9ce4cb2145e9a12f6fcabd8e79a57288
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-02-20 14:11:41 +0100

    fix: use full url for meta tags

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -10,18 +10,18 @@
   <!-- Google / Search Engine Tags -->
   <meta itemprop="name" content="Felte | Home">
   <meta itemprop="description" content="A form library for Svelte with simple error reporting.">
-  <meta itemprop="image" content="felte-logo-filled.png">
+  <meta itemprop="image" content="https://felte.dev/felte-logo-filled.png">
 
   <!-- Facebook Meta Tags -->
   <meta property="og:url" content="https://felte.dev">
   <meta property="og:type" content="website">
   <meta property="og:title" content="Felte | Home">
   <meta property="og:description" content="A form library for Svelte with simple error reporting.">
-  <meta property="og:image" content="felte-logo-filled.png">
+  <meta property="og:image" content="https://felte.dev/felte-logo-filled.png">
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="Felte | Home">
   <meta name="twitter:description" content="A form library for Svelte with simple error reporting.">
-  <meta name="twitter:image" content="felte-logo-filled.png">
+  <meta name="twitter:image" content="https://felte.dev/felte-logo-filled.png">
 </svelte:head>
